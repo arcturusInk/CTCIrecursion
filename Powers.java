da735eeee4f043913d2e2e4276bacdd58738c836
arcturusInk <arcturusInk@users.noreply.github.com>
2018-09-26 21:48:39 -0400

    raising integers to positive and negatvie powers

--- Powers.java
+++ Powers.java
@@ -1,6 +1,8 @@
 package com.company;
 
-/*Computing powers of a number using recursion*/
+/*Computing powers of a number using recursion.
+Write a recursive function that allows raising to a negative integer power as well.
+ */
 
 //Time Complexity: O(n)
 public class Powers {
@@ -15,9 +17,20 @@ public class Powers {
         return powers(number, pow-1) * number;
     }
 
+    public static double negativePower(int num, int exponent){
+        if(exponent == -1){
+            return (1.0/num);
+        }
+        return negativePower(num, exponent + 1) * (1.0/num);
+    }
+
     public static void main(String[] args) {
         int number = 4, pow = 3;
         //int number = 5, pow = 5;
-        System.out.print(powers(number,pow));
+        System.out.println(powers(number,pow));
+
+        //int num = 2, exponent = -3;
+        int num = 4, exponent = -2;
+        System.out.println(negativePower(num, exponent));
     }
 }
